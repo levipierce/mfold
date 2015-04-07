0d079151e65d007d182a32c7213c72cd84466744
levipierce <levipierce@gmail.com>
2015-04-07 09:27:59 -0700

    Added argparse interface

--- gen_files.py
+++ gen_files.py
@@ -5,6 +5,8 @@ import shutil
 import json
 import sys 
 import numpy as np
+import argparse
+
 
 
 def run(fname, seqs_to_gen, n_times_randomize_seq, mask=None):
@@ -15,7 +17,7 @@ def run(fname, seqs_to_gen, n_times_randomize_seq, mask=None):
 
     if mask:
         for key in mask:
-            ps = process_sequence(fname, seqs_to_gen, n_times_randomize_seq, (key,mask[key]))
+            ps = process_sequence(fname, seqs_to_gen, n_times_randomize_seq, (key, mask[key]))
             save_results(fname, seqs_to_gen, ps, mask_name=key)
     else:
         ps = process_sequence(fname, seqs_to_gen, n_times_randomize_seq)
@@ -25,20 +27,20 @@ def run(fname, seqs_to_gen, n_times_randomize_seq, mask=None):
 def process_sequence(fname, n_seqs_to_generate, n_times_randomize_seq, mask=None):
     sample = {}
     for i in range(1, n_seqs_to_generate+1):
-        fname_out ='%04d_%s' % (i, fname)
+        fname_out = '%04d_%s' % (i, fname)
         seq_name = process(fname, fname_out, n_times_randomize_seq, mask)
         base, ext = os.path.splitext(fname)
         if mask:
-            fname_out ='%04d_%s_mask_%s%s' % (i, base, mask[0], ext)
+            fname_out = '%04d_%s_mask_%s%s' % (i, base, mask[0], ext)
 
-        dir_name = "%04d"%i
-        cmd = "python process_fasta.py --fasta_file %s --scratch_dir %s"% (fname_out, dir_name)
+        dir_name = "%04d" % i
+        cmd = "python process_fasta.py --fasta_file %s --scratch_dir %s" % (fname_out, dir_name)
         print cmd
         #with open(os.devnull, 'w') as FNULL:
         #subprocess.call(cmd, shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
         subprocess.call(cmd, shell=True, stderr=subprocess.STDOUT)
         
-        energy_file_name = "%s_energy.dat"%dir_name
+        energy_file_name = "%s_energy.dat" % dir_name
         shutil.move("master_energy.dat", energy_file_name)
         #TODO Make this an option
         #shutil.rmtree(dir_name)
@@ -46,14 +48,21 @@ def process_sequence(fname, n_seqs_to_generate, n_times_randomize_seq, mask=None
             for l in fh:
                 l_s = l.split()
                 try:
-                    sample[i].append((float(l_s[1]),float(l_s[2])))
+                    sample[i].append((float(l_s[1]), float(l_s[2])))
                 except KeyError:
-                    sample[i] = [(float(l_s[1]),float(l_s[2]))]
+                    sample[i] = [(float(l_s[1]), float(l_s[2]))]
 
     return sample
 
     
 def save_results(fname, n_seqs_to_generate, sample, mask_name=None):
+    """
+    :param fname:
+    :param n_seqs_to_generate:
+    :param sample:
+    :param mask_name:
+    :return:
+    """
 
 
     m_a = np.array([s[0] for s in sample[1]])
@@ -61,13 +70,14 @@ def save_results(fname, n_seqs_to_generate, sample, mask_name=None):
     for i in range(2, n_seqs_to_generate+1):
         n_a = np.array([s[0] for s in sample[i]])
         n_b = np.array([s[1] for s in sample[i]])
-        m_a = np.vstack((m_a,n_a))
-        m_b = np.vstack((m_b,n_b))
+        m_a = np.vstack((m_a, n_a))
+        m_b = np.vstack((m_b, n_b))
 
     m_a_mean_of_segments = m_a.mean(0)
     m_b_mean_of_segments = m_b.mean(0)
-    m_a_mean_of_samples = m_a.mean(1)
-    m_b_mean_of_samples = m_b.mean(1)
+    if n_seqs_to_generate != 1:
+        m_a_mean_of_samples = m_a.mean(1)
+        m_b_mean_of_samples = m_b.mean(1)
 
     fname_base = os.path.splitext(fname)[0]
     if mask_name:
@@ -79,22 +89,26 @@ def save_results(fname, n_seqs_to_generate, sample, mask_name=None):
     with open(save_name, "w") as fh:
         json.dump(sample, fh)
         fh.write("\n")
-        json.dump(m_a_mean_of_segments.tolist(),fh)
+        json.dump(m_a_mean_of_segments.tolist(), fh)
         fh.write("\n")
-        json.dump(m_b_mean_of_segments.tolist(),fh)
+        json.dump(m_b_mean_of_segments.tolist(), fh)
         fh.write("\n")
-        json.dump(["%3f" % _ for _ in m_a_mean_of_samples.tolist()],fh)
-        fh.write("\n")
-        json.dump(m_b_mean_of_samples.tolist(),fh)
+        if n_seqs_to_generate != 1:
+            json.dump(["%3f" % _ for _ in m_a_mean_of_samples.tolist()], fh)
+            fh.write("\n")
+            json.dump(["%3f" % _ for _ in m_b_mean_of_samples.tolist()], fh)
+        else:
+            json.dump(["0.00"], fh)
+            fh.write("\n")
+            json.dump(["0.00"], fh)
 
-
-
-if __name__=='__main__':
-    fname_in = sys.argv[1]
-    seqs_to_gen = int(sys.argv[2])
-    num_times_to_randomize = int(sys.argv[3])
-
+def read_mask(file_name):
+    """
+    :param file_name: name of the file containing the mask
+    :return: dictionary of masked regions
+    """
     '''
+    Sample mask file...
 
     HSE1:		31795312-31795325
     HSE2:		31795405-31795418
@@ -106,20 +120,43 @@ if __name__=='__main__':
 
     '''
     mask={}
-    with open("HSPA1B_freezesites.txt", "r") as fh:
+    with open(file_name, "r") as fh:
         for l in fh:
             ls = l.split()
             name = ls[0].strip(":")
             s = int(ls[1].split("-")[0])
             e = int(ls[1].split("-")[1])
             try:
-                mask[name].append((s,e))
+                mask[name].append((s, e))
             except KeyError:
-                mask[name] = [(s,e)]
+                mask[name] = [(s, e)]
+    return mask
 
-        
-    run(fname_in, seqs_to_gen, num_times_to_randomize, 
-        mask=mask)
+
+def parse_it():
+    parser = argparse.ArgumentParser(description='Fold randomized fasta file.')
+    parser.add_argument(dest='fasta_file_name',
+                        help='The name of the fasta file.')
+    parser.add_argument('--num', dest='num_seqs_to_generate', type=int,
+                        help='The number of randomized sequences to generate.', default=1)
+    parser.add_argument('--num_rand', dest='num_rand',
+                        help='The number of times to randomize a sequence.', default=1)
+    parser.add_argument('--mask_file', dest='mask_file_name',
+                        help='File containing regions masks.', default=None)
+    parser.add_argument('--scratch_dir', dest='scratch_dir',
+                        help='The name of the scratch dir', default=os.getcwd())
+    return parser.parse_args()
+
+
+if __name__=='__main__':
+    args = parse_it()
+    fasta_file_name = args.fasta_file_name
+    seqs_to_gen = args.num_seqs_to_generate
+    num_times_to_randomize = args.num_rand
+    mask_file_name = args.mask_file_name
+
+
+    run(fasta_file_name, seqs_to_gen, num_times_to_randomize, mask=mask_file_name)
     #run(fname_in, seqs_to_gen, num_times_to_randomize, 
     #    mask={"mask_test3":[(133450052,133450052+300), 
     #        (133450052+301,133450052+601)]})
